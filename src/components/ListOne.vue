f0ba541ca57814941e4e7a2aedf1dcd8972a8b27
Sowmya R <sowmyas@baasha.net>
2017-11-17 06:08:24 -0500

    Add files via upload

---
+++ src/components/ListOne.vue
@@ -0,0 +1,36 @@
+// ListOne.vue
+<template>
+  <div class="container">
+    <div class="row fixed-top">
+        <h2>{{title}}</h2>
+    </div>
+    <div class="row">
+        <div v-for="item in saleProducts" :key="item.name">
+            <div class="product-list1">
+                <p class="item">{{item.name}} &nbsp; &nbsp; ${{item.price}}</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListOne",
+  data() {
+    return {
+      title: "List one : sale",
+    }
+  },
+  computed: {
+    /**@prop -computed 
+     * @description - The store data can be accessed through the computed property */
+    products() {
+      return this.$store.state.products;
+    },
+    saleProducts() {
+       return this.$store.getters.saleProducts;
+    }
+  }
+};
+</script>
---
+++ src/components/ListTwo.vue
@@ -0,0 +1,34 @@
+// ListTwo.vue
+<template>
+  <div class="container">
+    <div class="row fixed-top">
+        <h2>List Two.</h2>
+    </div>
+    <div class="row">
+        <div v-for="item in products" :key="item.name">
+            <div class="product-list2">
+                <p class="item">{{item.name}} &nbsp; &nbsp; ${{item.price}}</p>
+            </div>
+        </div>
+    </div>
+    <!-- Create a button to reduce the price-->
+    <button class="button" @click="reducedPrice(4)"> Reduce Price</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ListTwo",
+    computed: {
+    products() {
+      return this.$store.state.products;
+    }
+  },
+  methods: {
+      reducedPrice(value) {
+          /** Trigger the action defined in the store.  instead of mutation to handle async task*/
+          this.$store.dispatch('reducedPrice', value);
+      }
+  }
+}
+</script>
