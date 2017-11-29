3a3d8d61f87341e8ec827d968e7e6ec05760e791
Sowmya R <sowmyas@baasha.net>
2017-11-29 11:24:32 -0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     <div class="nav nav-horizontal fixed-top">
         <ul>
             <li><router-link class="item" to="/">Home</router-link> </li>
-            <li><router-link class="item" to="/2">List2</router-link> </li>
+            <li><router-link class="item" to="/2/Product1">List2</router-link> </li>
+            <li><router-link class="item" to="/3/Product3">List3</router-link> </li>
         </ul>
     </div>
     <div class="row">
@@ -17,16 +18,16 @@
 </template>
 
 <script>
-import ListOne from './components/ListOne.vue';
-import ListTwo from './components/ListTwo.vue';
 export default {
     name: "App",
-    components: {
-        'List-One':ListOne,
-        'List-Two':ListTwo
-    },
-    data(){
-        return { }
+    data() {
+        return {
+            obj_list:{
+                name:'Sample item name',
+                id: 2,
+                value: '$20.00'
+            }
+         }
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -7,11 +7,24 @@ import store from './store/store.js';
 /** Components */
 import ListOne from './components/ListOne.vue';
 import ListTwo from './components/ListTwo.vue';
+import ListThree from './components/ListThree.vue';
 
 Vue.use(VueRouter);
+
+function getObject(route) {
+    /** this one maps the router parameter to the corresponding object to be passed to the prop */
+    let _input = {
+        type: route.params.objectParam,
+        id: route.params.product_id
+    };
+    return {
+        page_obj: store.getters.getProduct(_input)
+    };
+}
 const routes = [
     { path: "/", component: ListOne },
-    { path: "/2", component: ListTwo }
+    { path: "/2/:id", component: ListTwo }, // routes with parameter.
+    { path: "/3/:objectParam/:product_id", component: ListThree, props: getObject }
 ];
 
 const router = new VueRouter({
